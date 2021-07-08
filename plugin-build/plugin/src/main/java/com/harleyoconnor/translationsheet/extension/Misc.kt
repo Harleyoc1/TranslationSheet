1013f180f0f6a2be7e1a249fafebff88c426a197
Harleyoc1 <harley.oconnor@icloud.com>
2021-07-09 00:48:20 +0100

    Create directory as well as file.

--- plugin-build/plugin/src/main/java/com/harleyoconnor/translationsheet/extension/Misc.kt
+++ plugin-build/plugin/src/main/java/com/harleyoconnor/translationsheet/extension/Misc.kt
@@ -3,9 +3,15 @@ package com.harleyoconnor.translationsheet.extension
 import org.gradle.api.file.Directory
 import org.gradle.api.file.RegularFile
 import org.gradle.api.provider.Provider
+import java.io.File
+
+fun File.createDirs(): File {
+    this.mkdirs()
+    return this
+}
 
 fun <F : RegularFile> Provider<F>.createFile(): Provider<F> {
-    this.orNull?.asFile?.createNewFile()
+    this.orNull?.asFile?.createDirs()?.createNewFile()
     return this
 }
 
